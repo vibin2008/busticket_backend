60539634bf2aea982de8c48fdcc37ed20c271731
Vibin <vibinvigneshs@gmail.com>
2025-11-13 17:21:24 +0530

    dsvsdv

--- app.py
+++ app.py
@@ -151,4 +151,5 @@ def status():
 
   
 if __name__=='__main__': 
-   app.run(debug=True)
+   port = int(os.environ.get("PORT", 5000))  # use PORT from Railway/Heroku
+   app.run(host="0.0.0.0", port=port, debug=True)
